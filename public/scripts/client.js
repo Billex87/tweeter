701d4de85cd5b87037816bdb9e23119ea2f73ff9
Billy <billy_mckinnon@hotmail.com>
2021-01-27 02:24:03 +0000

    public/index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,11 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+// /*
+//  * Client-side JS logic goes here
+//  * jQuery is already loaded
+//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+//  */
+// document.addEventListener("dblclick", (event) => {
+//   console.log(event);
+// });
+// document.addEventListener("keypress", (event) => {
+//   console.log(event);
+// });
