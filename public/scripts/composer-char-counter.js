4a14c2392df88f478f9fb3eb2a97e055ab006f66
Billy <billy_mckinnon@hotmail.com>
2021-01-28 19:24:22 +0000

    Added Errors For Invalid Tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-  $('#tweet-text').on('input', function(event) {
-  const charsLeft = (140 - $(this).val().length);
-  const outputTag = $(this).siblings('.count').children(".counter");
-  outputTag.text(charsLeft);
-  if (charsLeft < 0) { 
-    alert("Too Many Characters")
-    outputTag.addClass("redChars");
-  } else {outputTag.removeClass("redChars")
-  };
-  this.style.height = 'auto';
-  this.style.height = this.scrollHeight + 'px';
-})
+$(document).ready(function () {
+  $('#tweet-text').on('input', function (event) {
+    const charsLeft = (140 - $(this).val().length);
+    const outputTag = $(this).siblings('.count').children(".counter");
+    outputTag.text(charsLeft);
+    if (charsLeft < 0) {
+      outputTag.addClass("redChars");
+    } else {
+      outputTag.removeClass("redChars")
+    };
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+  })
 });
